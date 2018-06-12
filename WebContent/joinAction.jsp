e9cdb777a5a246b92c0e4ae730e1393f5da2b59e
jeenie <jeenie828@gmail.com>
2018-06-12 20:57:43 +0900

    Implement_Join

--- WebContent/joinAction.jsp
+++ WebContent/joinAction.jsp
@@ -1,12 +1,44 @@
-<%@ page language="java" contentType="text/html; charset=EUC-KR"
-    pageEncoding="EUC-KR"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page import= "java.io.*, net.bbs.UserDAO" %>
+<% request.setCharacterEncoding("UTF-8"); %>
+<jsp:useBean id="user" class="net.bbs.User" scope="page"/>
+<jsp:setProperty name="user" property="userID"/>
+<jsp:setProperty name="user" property="userPassword"/>
+<jsp:setProperty name="user" property="userName"/>
+<jsp:setProperty name="user" property="userGender"/>
+<jsp:setProperty name="user" property="userEmail"/>
+<!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
-<title>Insert title here</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>JSP 게시판 웹 사이트</title>
 </head>
 <body>
-
+	<%
+		if(user.getUserID() == null || user.getUserPassword() == null || user.getUserName() == null || user.getUserGender() == null || user.getUserEmail() == null) {
+			PrintWriter script = response.getWriter();
+			script.println("<script>");
+			script.println("alert('입력이 안 된 사항이 있습니다.')");
+			script.println("history.back()");
+			script.println("</script>");
+		} else {
+			UserDAO userDAO = new UserDAO();
+			int result = userDAO.join(user);
+			if (result == -1) {
+				PrintWriter script = response.getWriter();
+				script.println("<script>");
+				script.println("alert('이미 존재하는 아이디입니다.')");
+				script.println("history.back()");
+				script.println("</script>");
+			} else {
+				PrintWriter script = response.getWriter();
+				script.println("<script>");
+				script.println("location.href = 'main.jsp'");
+				script.println("</script>");
+			}
+		}
+		 
+	%>
 </body>
 </html>
--- src/net/bbs/UserDAO.java
+++ src/net/bbs/UserDAO.java
@@ -3,23 +3,44 @@ package net.bbs;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import javax.naming.NamingException;
 
 public class UserDAO {
-	public int login(String userID, String userPassword) throws Exception{
-		String sql = "SELECT * FROM user WHERE userID = ?";
-		try(Connection connection = DB.getConnection("BBS");
-			PreparedStatement statement = connection.prepareStatement(sql)) {
-				statement.setString(1, userID);
-				try(ResultSet resultSet = statement.executeQuery()) {
-					if(resultSet.next()) {
-						if(resultSet.getString(1).equals(userPassword))
-							return 1; // 로그인 성공
-						else 
-							return 0; // 비밀번호 불일치
-					}
-					return -1; //id 없음
+	public int login(String userID, String userPassword) throws Exception {
+		String sql = "SELECT userPassword FROM user WHERE userID = ?";
+		try (Connection connection = DB.getConnection("BBS");
+				PreparedStatement statement = connection.prepareStatement(sql)) {
+			statement.setString(1, userID);
+			try (ResultSet resultSet = statement.executeQuery()) {
+				if (resultSet.next()) {
+					if (resultSet.getString(1).equals(userPassword))
+						return 1; // 로그인 성공
+					else
+						return 0; // 비밀번호 불일치
 				}
+				return -1; // id 없음
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -2;
+	}
+
+	public int join(User user) {
+		String sql = "INSERT INTO USER VALUES (?, ?, ?, ?, ?)";
+		try (Connection connection = DB.getConnection("BBS");
+				PreparedStatement statement = connection.prepareStatement(sql)) {
+			statement.setString(1, user.getUserID());
+			statement.setString(2, user.getUserPassword());
+			statement.setString(3, user.getUserName());
+			statement.setString(4, user.getUserGender());
+			statement.setString(5, user.getUserEmail());
+			return statement.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1;
 	}
 }
